1355ed726cafb56231803e0bcbca43138d809449
Robert <rw@roko.li>
2021-09-27 10:44:58 +0200

    feat: Add capsules and contents to admin

--- memoorje/admin.py
+++ memoorje/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from memoorje.models import User
+from memoorje.models import Capsule, CapsuleContent, User
 
+admin.site.register(Capsule)
+admin.site.register(CapsuleContent)
 admin.site.register(User)
