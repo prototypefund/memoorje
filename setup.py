dbfaf4be3dbdf14cf08d3e6774b6120f2216dcbc
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2021-10-26 20:05:26 +0200

    chore: fix tests

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "django-downloadview~=2.1.1",
         "django-filter~=2.4.0",
         "django-rest-registration~=0.6.4",
-        "djeveric@https://git.hack-hro.de/memoorje/djeveric.git",
+        "djeveric",
         "drf-spectacular~=0.18.2",
         "memoorje_crypto",
     ],
