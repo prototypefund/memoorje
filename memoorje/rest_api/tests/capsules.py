67b2c202af746fd79d73604f08e8be9bdefff860
Robert <rw@roko.li>
2021-11-12 11:35:14 +0100

    chore: Switch to one-line comments

--- memoorje/rest_api/tests/capsules.py
+++ memoorje/rest_api/tests/capsules.py
@@ -9,17 +9,13 @@ from memoorje.tests.mixins import CapsuleMixin
 
 class CapsuleTestCase(CapsuleMixin, MemoorjeAPITestCase):
     def test_create_capsule_unauthorized(self) -> None:
-        """
-        Create a capsule unauthorized
-        """
+        """Create a capsule unauthorized."""
         url = "/capsules/"
         response = self.client.post(self.get_api_url(url))
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_create_capsule(self) -> None:
-        """
-        Create a capsule
-        """
+        """Create a capsule."""
         url = "/capsules/"
         self.authenticate_user()
         response = self.client.post(self.get_api_url(url), data={"name": "test", "description": "test"})
@@ -28,18 +24,14 @@ class CapsuleTestCase(CapsuleMixin, MemoorjeAPITestCase):
         self.assertEqual(Capsule.objects.get().owner, self.user)
 
     def test_retrieve_capsule_unauthorized(self) -> None:
-        """
-        Retrieve a capsule unauthorized
-        """
+        """Retrieve a capsule unauthorized."""
         url = "/capsules/{id}/"
         self.create_capsule()
         response = self.client.post(self.get_api_url(url, id=self.capsule.id))
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_retrieve_capsule(self) -> None:
-        """
-        Retrieve a capsule
-        """
+        """Retrieve a capsule."""
         url = "/capsules/{id}/"
         self.create_capsule()
         self.authenticate_user()
@@ -58,9 +50,7 @@ class CapsuleTestCase(CapsuleMixin, MemoorjeAPITestCase):
         )
 
     def test_retrieve_others_capsule(self) -> None:
-        """
-        Retrieve a capsule which does not belong to the logged in user
-        """
+        """Retrieve a capsule which does not belong to the logged in user."""
         url = "/capsules/{pk}/"
         other_capsule = self.create_capsule()
         self.create_user()
@@ -70,9 +60,7 @@ class CapsuleTestCase(CapsuleMixin, MemoorjeAPITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_update_capsule(self) -> None:
-        """
-        Update a capsule's name and description.
-        """
+        """Update a capsule's name and description."""
         url = "/capsules/{pk}/"
         new_name = "Changed name"
         new_description = "Changed description"
@@ -91,9 +79,7 @@ class CapsuleTestCase(CapsuleMixin, MemoorjeAPITestCase):
         self.assertEqual(new_description, self.capsule.description)
 
     def test_list_capsules(self):
-        """
-        List a user's capsules.
-        """
+        """List a user's capsules."""
         url = "/capsules/"
         self.create_capsule()
         self.authenticate_user()
@@ -114,9 +100,7 @@ class CapsuleTestCase(CapsuleMixin, MemoorjeAPITestCase):
         )
 
     def test_delete_capsule(self):
-        """
-        Delete a capsule.
-        """
+        """Delete a capsule."""
         url = "/capsules/{pk}/"
         self.create_capsule()
         self.authenticate_user()
