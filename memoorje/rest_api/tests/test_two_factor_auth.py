1fe26c38e395ec2270a9b7f714ac1cb617aaf272
Robert <rw@roko.li>
2022-01-07 13:54:57 +0100

    feat: Allow login with valid token
    Refs #15

--- memoorje/rest_api/tests/test_two_factor_auth.py
+++ memoorje/rest_api/tests/test_two_factor_auth.py
@@ -6,9 +6,9 @@ from memoorje.tests.mixins import UserMixin
 
 class TwoFactorUserTestCase(UserMixin, APITestCase):
     def test_login_without_token(self) -> None:
-        """Trying to login to a 2FA account without token returns an error."""
+        """Trying to log in to a 2FA account without token returns an error."""
         url = "/api/auth/login/"
-        self.create_user(two_factor=True)
+        self.create_user(is_2fa_enabled=True)
         data = {"login": self.email, "password": self.password}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -16,11 +16,20 @@ class TwoFactorUserTestCase(UserMixin, APITestCase):
         self.assertFalse(response.wsgi_request.user.is_authenticated)
 
     def test_login_with_invalid_token(self) -> None:
-        """Trying to login to a 2FA account with an invalid token returns an error."""
+        """Trying to log in to a 2FA account with an invalid token returns an error."""
         url = "/api/auth/login/"
-        self.create_user(two_factor=True)
+        self.create_user(is_2fa_enabled=True)
         data = {"login": self.email, "password": self.password, "token": "012345"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["detail"].code, "login-invalid")
         self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+    def test_login_with_valid_token(self) -> None:
+        """Trying to log in to a 2FA account with a valid token logs the user in successfully."""
+        url = "/api/auth/login/"
+        self.create_user(is_2fa_enabled=True)
+        data = {"login": self.email, "password": self.password, "token": self.two_factor_token}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
