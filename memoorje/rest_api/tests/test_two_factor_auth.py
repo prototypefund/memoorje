f4fd6d562a7caed9142f2b6b8410caf283b79290
Robert <rw@roko.li>
2022-01-07 12:11:42 +0100

    feat: Invalid token returns error on login
    Refs #15

--- memoorje/rest_api/tests/test_two_factor_auth.py
+++ memoorje/rest_api/tests/test_two_factor_auth.py
@@ -14,3 +14,13 @@ class TwoFactorUserTestCase(UserMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["non_field_errors"][0].code, "provide-token")
         self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+    def test_login_with_invalid_token(self) -> None:
+        """Trying to login to a 2FA account with an invalid token returns an error."""
+        url = "/api/auth/login/"
+        self.create_user(two_factor=True)
+        data = {"login": self.email, "password": self.password, "token": "012345"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["detail"].code, "login-invalid")
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
--- memoorje_2fa/utils.py
+++ memoorje_2fa/utils.py
@@ -1,6 +1,15 @@
 from django.conf import settings
 from django_otp import devices_for_user
 from django_otp.plugins.otp_totp.models import TOTPDevice
+from rest_registration.exceptions import UserNotFound
+from rest_registration.utils.users import authenticate_by_login_data
+
+
+def authenticate(data, **kwargs):
+    user = authenticate_by_login_data(data, **kwargs)
+    if is_2fa_enabled_for_user(user):
+        raise UserNotFound()
+    return user
 
 
 def create_default_device_for_user(user):
