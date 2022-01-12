36ab775e575de184b3c0e9b6613495471a09a460
Robert <rw@roko.li>
2022-01-12 09:49:24 +0100

    feat: Validate token when enabling 2FA
    Refs #15

--- memoorje/rest_api/tests/test_two_factor_auth.py
+++ memoorje/rest_api/tests/test_two_factor_auth.py
@@ -1,3 +1,7 @@
+from binascii import unhexlify
+
+from django_otp.oath import TOTP
+from django_otp.plugins.otp_totp.models import default_key
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -35,10 +39,39 @@ class TwoFactorUserTestCase(UserMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(response.wsgi_request.user.is_authenticated)
 
-    def test_enable_2fa(self):
-        """2FA can be enabled by posting to the enable_2fa endpoint for an authenticated user."""
+    def test_enable_2fa_when_already_enabled(self):
+        """Trying to enable 2FA when it is already enabled returns an error."""
         url = "/api/auth/two-factor/"
+        self.create_user(is_2fa_enabled=True)
         self.authenticate_user()
         response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["user"][0].code, "already-enabled")
+
+    def test_enable_2fa_unauthorized(self):
+        """Enabling 2FA when not logged in returns an error."""
+        url = "/api/auth/two-factor/"
+        self.create_user()
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertFalse(is_2fa_enabled_for_user(self.user))
+
+    def test_enable_2fa_with_invalid_token(self):
+        """Enabling 2FA with an invalid combination of key/ token fails."""
+        url = "/api/auth/two-factor/"
+        data = {"key": "0123456789ABCDEF0123", "token": 123456}
+        self.authenticate_user()
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["non_field_errors"][0].code, "invalid-token")
+        self.assertFalse(is_2fa_enabled_for_user(self.user))
+
+    def test_enable_2fa_with_valid_token(self):
+        """Enabling 2FA with a valid combination of key/ token succeeds."""
+        url = "/api/auth/two-factor/"
+        key = default_key()
+        data = {"key": key, "token": TOTP(unhexlify(key.encode())).token()}
+        self.authenticate_user()
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(is_2fa_enabled_for_user(self.user))
--- memoorje_2fa/serializers.py
+++ memoorje_2fa/serializers.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate
+from django.db import transaction
 from django_otp.plugins.otp_totp.models import TOTPDevice
 from rest_framework import serializers
+from rest_framework.exceptions import ErrorDetail
 
-from memoorje_2fa.utils import get_token_max_value
+from memoorje.models import User
+from memoorje_2fa.utils import get_token_max_value, get_totp_for_device
 from memoorje_2fa.users import create_default_device_for_user, is_2fa_enabled_for_user
 
 
@@ -21,11 +24,25 @@ class TwoFactorLoginSerializer(serializers.Serializer):
 
 
 class TwoFactorSerializer(serializers.HyperlinkedModelSerializer):
+    token = serializers.IntegerField(write_only=True, min_value=0, max_value=get_token_max_value())
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = TOTPDevice
-        fields = ["user"]
+        fields = ["key", "token", "user"]
 
     def create(self, validated_data):
-        return create_default_device_for_user(validated_data["user"])
+        with transaction.atomic():
+            device = create_default_device_for_user(validated_data["user"], key=validated_data["key"])
+            if not device.verify_token(validated_data["token"]):
+                # We raise a late ValidationError (after actual validation is already done). This is, as we cannot
+                # verify the token without creating the device (the device updates the drift upon verification).
+                raise serializers.ValidationError(
+                    {"non_field_errors": [ErrorDetail("Token is not valid for the given key", code="invalid-token")]}
+                )
+            return device
+
+    def validate_user(self, user: User):
+        if is_2fa_enabled_for_user(user):
+            raise serializers.ValidationError("2FA is already enabled for this user", code="already-enabled")
+        return user
