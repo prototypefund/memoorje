e0128be592078b68734c181f36ab142ef1b1bd4f
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2022-02-25 01:56:27 +0100

    packaging(docker): allow docker image users to override some settings

--- docker/memoorje_settings.py
+++ docker/memoorje_settings.py
@@ -9,3 +9,11 @@ DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 STATIC_ROOT = "/app/static"
 MEDIA_ROOT = "/var/lib/memoorje/media"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+ORIGIN = os.environ.get("MEMOORJE_ORIGIN", ORIGIN)
+USE_X_FORWARDED_HOST = True
+
+EMAIL_HOST = os.environ.get("MEMOORJE_EMAIL_HOST", "localhost")
+EMAIL_PORT = int(os.environ.get("MEMOORJE_EMAIL_PORT", 25))
+EMAIL_USE_TLS = os.environ.get("MEMOORJE_EMAIL_USE_TLS", "False") == "True"
+DEFAULT_FROM_EMAIL = os.environ.get("MEMOORJE_DEFAULT_FROM_EMAIL", "noreply@example.com")
