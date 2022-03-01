907aa6b5a8fe6dbd64da84d9416982c41ec78d75
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2022-03-01 01:54:08 +0100

    fix: don’t reuse __version__ from module
    We don’t want to enforce an __init__.py without any imports but setup.py
    must be runnable even before the module is installed.

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ import os
 
 from setuptools import find_namespace_packages, setup
 
-from memoorje import __version__
-
 __dir__ = os.path.abspath(os.path.dirname(__file__))
+__version__ = "0.1.0"
 
 try:
     with open(os.path.join(__dir__, "README.md")) as f:
